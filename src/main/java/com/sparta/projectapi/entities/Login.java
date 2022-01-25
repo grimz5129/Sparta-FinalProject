bedeb1e60f4801bd9ea67c17ff84f6c0c66c7cf1
djfrigid <electromagneticsnake@hotmail.co.uk>
2022-01-25 16:16:01 +0000

    Login System Initial Commit

---
+++ src/main/java/com/sparta/projectapi/entities/Login.java
@@ -0,0 +1,42 @@
+package com.sparta.projectapi.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "logins")
+public class Login {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "username", length = 128)
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
