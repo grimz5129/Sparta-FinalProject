299539da69d1a0ee20430b26fed1725fbe73c7d6
Yefri <yefrivelasquez@gmail.com>
2022-01-28 11:51:14 +0000

    Added my tests with the latest branch

--- app/src/test/java/com/sparta/projectapi/ListControllerTests.java
+++ app/src/test/java/com/sparta/projectapi/ListControllerTests.java
@@ -1,6 +1,52 @@
 package com.sparta.projectapi;
 
-public class ListControllerTests {
+import com.sparta.projectapi.requests.RequestFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class ListControllerTests {
+    private String newListResponse;
+    private String noAuthResponse;
+    private String addItemResponse;
+    private String viewListResponse;
+
+    @Test
+    @DisplayName("POST check list create response")
+    public void checkCreateListResponse() throws IOException, URISyntaxException, InterruptedException {
+        String username = "Yefri51";
+        newListResponse = RequestFactory.listRequest("/list/create/newlist",username, "BqwkosRkCpavC3cvgUpweXLsYHUWVC1pi1yP6zrn","POST","NewList.json");
+        Assertions.assertTrue(newListResponse.equals("Blank List Created for User: " + username + ". Please send items to the correct endpoint to add them."));
+    }
+
+    @Test
+    @DisplayName("POST no auth list response")
+    public void checkNotAuthResponse() throws IOException, URISyntaxException, InterruptedException {
+        String username = "Yefri51";
+        noAuthResponse = RequestFactory.listRequest("/list/create/newlist",username, "BqwkosRkCpavC3cvgUadwadwaWVC1pi1yP6zrn","POST","NewList.json");
+        Assertions.assertTrue(noAuthResponse.equals("You are not authorized for this page, Check your username or token and try again."));
+    }
+
+    @Test
+    @DisplayName("POST add item response")
+    public void addItemToListResponse() throws IOException, URISyntaxException, InterruptedException {
+        String username = "Yefri51";
+        int id = 1;
+        addItemResponse = RequestFactory.listRequest("/list/create/newitems/" + id, username, "BqwkosRkCpavC3cvgUpweXLsYHUWVC1pi1yP6zrn","POST","AddItemToList.json");
+        System.out.println(addItemResponse);
+        Assertions.assertTrue(addItemResponse.equals("All items successfully added to the list. "));
+    }
+
+    @Test
+    @DisplayName("GET view login response")
+    public void viewListResponse() throws IOException, URISyntaxException, InterruptedException {
+        String username = "Yefri51";
+        int id = 1;
+        viewListResponse = RequestFactory.listRequest("/list/view/" + id, username, "BqwkosRkCpavC3cvgUpweXLsYHUWVC1pi1yP6zrn","GET",null);
+        Assertions.assertTrue(viewListResponse.equals("[{\"itemName\":\"Docker\",\"itemType\":\"Application\",\"id\":\"1\"},{\"itemName\":\"Ruby\",\"itemType\":\"Package\",\"id\":\"2\"}]"));
+    }
 
 }
--- app/src/test/java/com/sparta/projectapi/json/UserController.java
+++ app/src/test/java/com/sparta/projectapi/json/UserController.java
@@ -1,4 +1,4 @@
-package com.sparta.projectapi.controllers;
+package com.sparta.projectapi.json;
 
 import com.sparta.projectapi.entities.User;
 import com.sparta.projectapi.repositories.LoginRepository;
@@ -42,8 +42,8 @@ public class UserController {
     public ResponseEntity<String> deleteUser(@RequestHeader("Authorization") String usernameAndAuthToken, @PathVariable int id) {
         String[] headerParts = usernameAndAuthToken.split(" ");
         if (authorizationService.checkValidToken(headerParts[1], headerParts[2])) {
-            userRepository.deleteById(id);
             loginRepository.deleteById(id);
+            userRepository.deleteById(id);
             return new ResponseEntity<>("User and credentials have been deleted", HttpStatus.OK);
         } else
             return new ResponseEntity<>("You are not authorized for this page, Check your username or token and try again.", HttpStatus.UNAUTHORIZED);
